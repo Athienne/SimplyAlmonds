ca89aba3c3c5831e52da7d2799ab1190cb8f3621
gus <mogugod@gmail.com>
2021-02-04 11:37:08 +0800

    .

--- SimplyAlmonds/AdminPages/AdminMaster.Master.designer.cs
+++ SimplyAlmonds/AdminPages/AdminMaster.Master.designer.cs
@@ -31,14 +31,5 @@ namespace SimplyAlmonds.AdminPages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
---
+++ SimplyAlmonds/Website (Front-End)/Account.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UserPages/UserMaster.Master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="SimplyAlmonds.Website__Front_End_.Account" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
