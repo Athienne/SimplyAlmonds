47e28ff48d6e142718780449c3338f2a9d101499
Michiru Suzumiya <mamamauag@live.mcl.edu.ph>
2021-02-18 09:50:34 +0800

    sdsdsd

--- SimplyAlmonds/UserPages/MasterUser.Master.designer.cs
+++ SimplyAlmonds/UserPages/MasterUser.Master.designer.cs
@@ -76,14 +76,5 @@ namespace SimplyAlmonds.UserPages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
-        /// <summary>
-        /// admin control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton admin;
     }
 }
